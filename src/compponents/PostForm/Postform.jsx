1fb025967bd3e4a80552021b836790098877f207
Fenilpanseriya <92420478+Fenilpanseriya@users.noreply.github.com>
2023-12-02 10:45:41 +0530

    Update Postform.jsx

--- src/compponents/PostForm/Postform.jsx
+++ src/compponents/PostForm/Postform.jsx
@@ -27,34 +27,7 @@ function Postform({ post ,content,title ,slug ,status}) {
         console.log("data is "+JSON.stringify(formdata));
         console.log((data.image[0].name))
         
-        // if (post) {
-        //     const file = data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null;
-
-        //     if (file) {
-        //         appwriteService.deleteFile(post.featuredImage);
-        //     }
-
-        //     const dbPost = await appwriteService.updatePost(post.$id, {
-        //         ...data,
-        //         featuredImage: file ? file.$id : undefined,
-        //     });
-
-        //     if (dbPost) {
-        //         navigate(`/post/${dbPost.$id}`);
-        //     }
-        // } else {
-        //     const file = await appwriteService.uploadFile(data.image[0]);
-
-        //     if (file) {
-        //         const fileId = file.$id;
-        //         data.featuredImage = fileId;
-        //         const dbPost = await appwriteService.createPost({ ...data, userId: userData.$id });
-
-        //         if (dbPost) {
-        //             navigate(`/post/${dbPost.$id}`);
-        //         }
-        //     }
-        // }
+       
         const response=await axios.post("http://localhost:6060/createPost",{
             image:data.image[0].name,
             content:data.content,
